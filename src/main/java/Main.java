dc01a122a57ecf63949d603ef7143c77bb907719
Enzo <drovandienzo@gmail.com>
2025-09-01 21:57:03 -0300

    feature: EJ4-Builder Method with lombock

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import main.java.builder.Usuario;
 import main.java.factory.Libro;
 import main.java.factory.LogisticaLibro;
 import main.java.singleton.Database;
@@ -20,5 +21,25 @@ public class Main {
 
         System.out.println("Libro físico: " + libroFisico.getTitulo() + " - " + libroFisico.getAutor());
         System.out.println("Libro digital: " + libroDigital.getTitulo() + " - " + libroDigital.getAutor());
+
+        //probar builder
+        System.out.println("\n--- PROBANDO BUILDER ---");
+        Usuario usuario1 = Usuario.builder()
+                .nombre("Miguel Borja")
+                .email("costillares@gmail.com")
+                .direccion("Area chica del Monumental 123")
+                .telefono("23412341")
+                .fechaNacimiento("26/01/1993")
+                .build();
+
+        Usuario usuario2 = Usuario.builder()
+                .nombre("Edinson Cavani")
+                .email("cavani@boca.com")
+                .direccion("Sección 'casi gol' de la Bombonera 321")
+                .fechaNacimiento("14/02/1987")
+                .build();
+
+        System.out.println("Ejemplo usuario 1: "+usuario1);
+        System.out.println("Ejemplo usuario 2: "+usuario2);
     }
 }
